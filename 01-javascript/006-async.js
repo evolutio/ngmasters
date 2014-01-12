68bca5e75527ec58a4aa2af9da967ab20a9d1f2a
Tony Lampada <tonylampada@gmail.com>
2014-01-12 08:35:08 -0200

    006 - Programacao assincrona

--- 01-javascript/006-async.js
+++ 01-javascript/006-async.js
@@ -27,9 +27,24 @@ function chega(){
 }
 
 function ajax_sim(){
-
+	var url = 'https://api.github.com/repos/freedomsponsors/www.freedomsponsors.org/issues'
+	$.get(url).success(function(result){
+		console.log('02 - do que isso!');
+		console.table(result);
+	});
+	console.log('01 - isso roda antes...');
 }
 
 function ajax_nao(){
-	
+	function getGithubIssues(){
+		var issues = [];
+		var url = 'https://api.github.com/repos/freedomsponsors/www.freedomsponsors.org/issues'
+		$.get(url).success(function(result){
+			issues = result;
+		});
+		return issues;
+	}
+
+	var issues = getGithubIssues();
+	console.table(issues); // quéééééén!
 }
