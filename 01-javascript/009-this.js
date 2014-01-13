313d26320e40220f314c1778373f6d3e0ba7ab02
Tony Lampada <tonylampada@gmail.com>
2014-01-13 00:01:16 -0200

    this

---
+++ 01-javascript/009-this.js
@@ -0,0 +1,55 @@
+var o = {name: 'Objeto'};
+o.alertnow = function(){
+	alert(this);
+}
+o.alertlater = function(){
+	setTimeout(function(){alert(this)}, 1000);
+}
+
+
+function ajax_faz_de_conta(msg, url, lineNumber){
+	console.log('Enviando erro pro servidor: '+url+':'+lineNumber+' - '+msg);
+}
+
+function dapau(){
+	var x = undefined;
+	x.do_somenthing();
+}
+
+function set_onerror1(){
+	function ErrorLogger(){
+		this.ja_loguei = {};
+	}
+
+	ErrorLogger.prototype.log_error = function(msg, url, lineNumber){
+		console.log('peguei o erro');
+		if(!this.ja_loguei[msg]){
+			ajax_faz_de_conta(msg, url, lineNumber);
+			this.ja_loguei[msg] = true;
+		}
+	}
+
+	var logger = new ErrorLogger()
+	window.onerror = logger.log_error;
+}
+
+function set_onerror2(){
+	function ErrorLogger(){
+		this.ja_loguei = {};
+	}
+
+	ErrorLogger.prototype.log_error_generator = function(){
+		var _this = this;
+		var log_error = function(msg, url, lineNumber){
+			console.log('peguei o erro');
+			if(!_this.ja_loguei[msg]){
+				ajax_faz_de_conta(msg, url, lineNumber);
+				_this.ja_loguei[msg] = true;
+			}
+		}
+		return log_error;
+	}
+
+	var logger = new ErrorLogger()
+	window.onerror = logger.log_error_generator();
+}
