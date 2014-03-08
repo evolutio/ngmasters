b0495e2b7d10c76e0df898a85fd8176d01167b5e
Tony Lampada <tonylampada@gmail.com>
2014-03-08 15:33:25 -0300

    Navegador de arquivos

--- js/github.js
+++ js/github.js
@@ -18,6 +18,18 @@ myapp.factory('Github', function($http){
         list_issue_comments: function(owner, repo, issue_number){
             var url="https://api.github.com/repos/"+owner+"/"+repo+"/issues/"+issue_number+"/comments";
             return $http.get(url);
+        },
+        get_contents: function(owner, repo, path){
+            var url="https://api.github.com/repos/"+owner+"/"+repo+"/contents"+path;
+            return $http.get(url);
+        },
+        decode_file_contents : function(contents_base64){
+            var lines_base_64 = contents_base64.split("\n");
+            var lines = [];
+            for(var i=0; i<lines_base_64.length; i++){
+                lines.push(atob(lines_base_64[i]));
+            }
+            return lines.join("");
         }
 	}
 })
