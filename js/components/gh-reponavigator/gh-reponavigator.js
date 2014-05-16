083c313ec48185fec58c8c81edd5dcd9c92658ac
tonylampada <tonylampada@gmail.com>
2014-05-16 20:50:21 -0300

    diretiva reponavigator

--- js/components/gh-reponavigator/gh-reponavigator.js
+++ js/components/gh-reponavigator/gh-reponavigator.js
@@ -1,14 +1,28 @@
 (function(){
-  angular.module('mod-gh-reponavigator', ['modgithubapi']).directive('ghReponavigator', function() {
+  angular.module('mod-gh-reponavigator', ['mod-gh-repochooser', 'modpopup', 'mod-gh-filetree']).directive('ghReponavigator', function() {
     return {
       restrict: 'E',
       replace: true,
       templateUrl: '/js/components/gh-reponavigator/gh-reponavigator.html',
-      scope: {
-          onchooserepo: '&'
-      },
+      scope: {},
       controller: ['$scope', 'Github', function ($scope, Github) {
         
+        $scope.escolheuorepo = function(repo){
+          $scope.selected_repo = repo;
+        };
+
+        $scope.abrearquivo = function(repo, file){
+          $scope.current_file = file;
+          if(file.file_contents){
+            $scope.showpopup = true;
+          } else {
+            Github.get_contents($scope.selected_repo.owner.login, $scope.selected_repo.name, file.path).success(function(result){
+              file.file_contents = Github.decode_file_contents(result.content);
+              $scope.showpopup = true;
+            });
+          }
+        }
+
       }]
     }
   });
