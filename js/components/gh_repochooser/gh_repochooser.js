d32770525404f8db5108aeb1b63543827d0c259e
tonylampada <tonylampada@gmail.com>
2015-05-19 20:14:05 -0300

    removendo codigo veio

--- js/components/gh_repochooser/gh_repochooser.js
+++ js/components/gh_repochooser/gh_repochooser.js
@@ -16,25 +16,11 @@
       controller: function ($scope, GithubModel, $timeout) {
         var gm = $scope.gm = GithubModel;
 
-        var DELAY = 800;
-        var timer;
-        $scope.searchowner_keyup = function(evt){
-          if(timer){
-            $timeout.cancel(timer)
-          }
-          timer = $timeout(function(){
-            if(gm.username_input){
-              gm.busca_usuarios_pelo_nome();
-            }
-            timer = null;
-          }, DELAY);
-        }
-
         //Populate repo combo
         $scope.selectowner = function(user){
           gm.escolhe_usuario(user);
           gm.busca_repos_do_usuario();
-        }
+        };
       }
     }
   });
